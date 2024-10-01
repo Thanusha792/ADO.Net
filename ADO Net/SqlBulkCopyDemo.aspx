7c0be73d693a0e88f8d214d38636e5d1f9103e70
thanusha thanu <stanu1998@gmail.com>
2024-10-01 17:27:07 -0500

    SQLBulkCopy Added

---
+++ ADO Net/SqlBulkCopyDemo.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="SqlBulkCopyDemo.aspx.cs" Inherits="ADO_Net.SqlBulkCopyDemo" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <div>
+    <asp:GridView ID="GridView1" runat="server" CellPadding="4" ForeColor="#333333" GridLines="None">
+        <AlternatingRowStyle BackColor="White" ForeColor="#284775" />
+        <EditRowStyle BackColor="#999999" />
+        <FooterStyle BackColor="#5D7B9D" Font-Bold="True" ForeColor="White" />
+        <HeaderStyle BackColor="#5D7B9D" Font-Bold="True" ForeColor="White" />
+        <PagerStyle BackColor="#284775" ForeColor="White" HorizontalAlign="Center" />
+        <RowStyle BackColor="#F7F6F3" ForeColor="#333333" />
+        <SelectedRowStyle BackColor="#E2DED6" Font-Bold="True" ForeColor="#333333" />
+        <SortedAscendingCellStyle BackColor="#E9E7E2" />
+        <SortedAscendingHeaderStyle BackColor="#506C8C" />
+        <SortedDescendingCellStyle BackColor="#FFFDF8" />
+        <SortedDescendingHeaderStyle BackColor="#6F8DAE" />
+    </asp:GridView>
+        </div>
+    <div>
+
+        <br />
+
+    </div>
+     <asp:Button ID="Button1" runat="server" Text="Click Here to copy" OnClick="Button1_Click" />
+     <br />
+    <div>
+            <br />
+            <br />
+            <asp:GridView ID="GridView2" runat="server" CellPadding="4" ForeColor="#333333" GridLines="None">
+                <AlternatingRowStyle BackColor="White" />
+                <EditRowStyle BackColor="#7C6F57" />
+                <FooterStyle BackColor="#1C5E55" Font-Bold="True" ForeColor="White" />
+                <HeaderStyle BackColor="#1C5E55" Font-Bold="True" ForeColor="White" />
+                <PagerStyle BackColor="#666666" ForeColor="White" HorizontalAlign="Center" />
+                <RowStyle BackColor="#E3EAEB" />
+                <SelectedRowStyle BackColor="#C5BBAF" Font-Bold="True" ForeColor="#333333" />
+                <SortedAscendingCellStyle BackColor="#F8FAFA" />
+                <SortedAscendingHeaderStyle BackColor="#246B61" />
+                <SortedDescendingCellStyle BackColor="#D4DFE1" />
+                <SortedDescendingHeaderStyle BackColor="#15524A" />
+            </asp:GridView>
+    </div>
+</asp:Content>
---
+++ ADO Net/SqlBulkCopyDemo.aspx.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ADO_Net
+{
+    public partial class SqlBulkCopyDemo : System.Web.UI.Page
+    {
+        String connectionS = ConfigurationManager.ConnectionStrings["csTOProduct"].ConnectionString;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack) // Load data only on the initial page load
+            {
+                LoadSourceData();
+            }
+
+            if (Request.Form[Button1.UniqueID] != null) // Check if Button1 is clicked
+            {
+                PerformBulkCopy();
+            }
+        }
+
+        private void LoadSourceData()
+        {
+            using (SqlConnection con = new SqlConnection(connectionS))
+            {
+                con.Open();
+                SqlCommand sourceDataCommand = new SqlCommand("SELECT ProductID, ProductName, Price FROM ProductSource;", con);
+                SqlDataReader sourceReader = sourceDataCommand.ExecuteReader();
+                GridView1.DataSource = sourceReader;
+                GridView1.DataBind();
+                sourceReader.Close();
+            }
+        }
+
+        private void PerformBulkCopy()
+        {
+            using (SqlConnection con = new SqlConnection(connectionS))
+            {
+                con.Open();
+
+                // Read source data into a DataTable
+                DataTable dataTable = new DataTable();
+                using (SqlCommand sourceDataCommand = new SqlCommand("SELECT ProductID, ProductName, Price FROM ProductSource;", con))
+                {
+                    using (SqlDataReader sourceReader = sourceDataCommand.ExecuteReader())
+                    {
+                        dataTable.Load(sourceReader);
+                    }
+                }
+
+                // Perform bulk copy to the destination table
+                using (SqlConnection destinationConnection = new SqlConnection(connectionS))
+                {
+                    destinationConnection.Open();
+                    using (SqlBulkCopy bulkCopy = new SqlBulkCopy(destinationConnection))
+                    {
+                        bulkCopy.DestinationTableName = "dbo.ProductDestination";
+
+                        try
+                        {
+                            // Write from the source DataTable to the destination.
+                            bulkCopy.WriteToServer(dataTable);
+                            Response.Write("Data copied to DestinationTable successfully.<br/>");
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine("Error: " + ex.Message);
+                            Response.Write("Data not copied to DestinationTable successfully.<br/>");
+                        }
+                    }
+
+                    // Check and display the data from the destination table
+                    SqlCommand commandCheckData = new SqlCommand("SELECT ProductID, ProductName, Price FROM ProductDestination;", destinationConnection);
+                    SqlDataReader destinationReader = commandCheckData.ExecuteReader();
+
+                    GridView2.DataSource = destinationReader;
+                    GridView2.DataBind();
+
+                    destinationReader.Close();
+                }
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            // Any additional logic for button click can be added here
+        }
+    }
+}
